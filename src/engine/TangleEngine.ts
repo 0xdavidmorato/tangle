d0283b7b5a8f0428559bcb8574ca30ba93712243
blueboxip <ipworks.com.pt@gmail.com>
2026-07-10 17:09:09 +0100

    feat(engine): implement timeline stage transitions

--- src/engine/TangleEngine.ts
+++ src/engine/TangleEngine.ts
@@ -10,12 +10,39 @@ export class TangleEngine implements Engine {
 
   public dispatch(event: EngineEvent): void {
     switch (event) {
-        case "timelineChange":
-        // TODO: atualizar a timeline narrativa
+      case "timelineChange":
+        this.advanceTimeline();
         break;
 
-        default:
+      default:
         break;
-        }
     }
+  }
+
+  private advanceTimeline(): void {
+    switch (this.currentStage) {
+      case "initialization":
+        this.currentStage = "introduction";
+        break;
+
+      case "introduction":
+        this.currentStage = "exploration";
+        break;
+
+      case "exploration":
+        this.currentStage = "focus";
+        break;
+
+      case "focus":
+        this.currentStage = "reflection";
+        break;
+
+      case "reflection":
+        this.currentStage = "conclusion";
+        break;
+
+      case "conclusion":
+        break;
+    }
+  }
 }
